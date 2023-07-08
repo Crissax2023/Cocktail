b20f2b039a7664356c997a91bfe6f93821c88084
Cristian <Crissax2020@gmail.com>
2023-07-08 13:53:49 -0600

    add app login signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,7 +8,9 @@ getLogin,
 postLogin
 }=require('../controllers/auth.controller')
 
-userRouter.get('/signup',getSignup)
+const{    isLoggedIn,    isLoggedOut} = require('../middleware/guard-auth.middleware')
+
+userRouter.get('/signup',isLoggedOut,getSignup)
 
 
 userRouter.post('/signup',postSignup)
@@ -16,7 +18,7 @@ userRouter.post('/signup',postSignup)
 
 
 
-userRouter.get('/profile',getProfile)
+userRouter.get('/profile',isLoggedIn,getProfile)
 
 
 
@@ -24,5 +26,16 @@ userRouter.get('/profile',getProfile)
 userRouter.get('/login', getLogin)
 userRouter.post('/login', postLogin)
 
+
+userRouter.get('/logout',(req,res,next)=>
+{
+     req.session.destroy((err)=>{
+        if(err)
+        {
+            return next(err)
+        }
+        res.redirect('/auth/profile')
+     })
+})
 
 module.exports = userRouter;
